f633d4c4115b103533eabc6583427fb884c53ce3
ezio84 <brabus84@gmail.com>
2018-11-07 23:48:51 +0100

    Launcher3: Add kill action to app shortcuts popup
    Squashed:
    
    Launcher3: Dismiss task from recents when killed
    
    Change-Id: I25f37484a444346bf08e6960893892cc4bf25ada
    
    Launcher3: Add FORCE_STOP_PACKAGES permission
    
    Change-Id: I20dd0287c47269ccfdd02a8eae2a3da1de96e206
    
    Launcher3: Update kill app button drawable
    
    Change-Id: Iff78ae0528b8ed9c7b11952352e4b6d43507e233
    Co-authored-by: Ali B <abittin@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Alexandru Scurtu <sasha.scurtu@gmail.com>
    [ghostrider-reborn: Add privapp permissions, adapt to Sv2]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- Android.mk
+++ Android.mk
@@ -39,8 +39,7 @@ LOCAL_PACKAGE_NAME := Launcher3Go
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep
-LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3
-LOCAL_REQUIRED_MODULES += privapp_whitelist_com.android.launcher3-ext.xml
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3 privapp_whitelist_com.android.launcher3-ext.xml privapp_whitelist_arrow_launcher3.xml
 
 LOCAL_FULL_LIBS_MANIFEST_FILES := \
     $(LOCAL_PATH)/AndroidManifest.xml \
@@ -73,8 +72,7 @@ LOCAL_PACKAGE_NAME := Launcher3QuickStep
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3
-LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3
-LOCAL_REQUIRED_MODULES += privapp_whitelist_com.android.launcher3-ext.xml
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3 privapp_whitelist_com.android.launcher3-ext.xml privapp_whitelist_arrow_launcher3.xml
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/quickstep/res
 
@@ -127,8 +125,7 @@ LOCAL_PACKAGE_NAME := Launcher3QuickStepGo
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep
-LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3
-LOCAL_REQUIRED_MODULES += privapp_whitelist_com.android.launcher3-ext.xml
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3 privapp_whitelist_com.android.launcher3-ext.xml privapp_whitelist_arrow_launcher3.xml
 
 LOCAL_FULL_LIBS_MANIFEST_FILES := \
     $(LOCAL_PATH)/go/AndroidManifest.xml \
