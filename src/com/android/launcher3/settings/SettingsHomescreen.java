crdroidandroid/android_packages_apps_Launcher3

Launcher3: Add sub-categories under home screen settings
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -177,12 +177,10 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
             setPreferencesFromResource(R.xml.launcher_home_screen_preferences, rootKey);
 
             PreferenceScreen screen = getPreferenceScreen();
-            for (int i = screen.getPreferenceCount() - 1; i >= 0; i--) {
-                Preference preference = screen.getPreference(i);
-                if (!initPreference(preference)) {
-                    screen.removePreference(preference);
-                }
-            }
+
+            mShowGoogleAppPref = screen.findPreference(KEY_ENABLE_MINUS_ONE);
+            mShowGoogleBarPref = screen.findPreference(Utilities.KEY_DOCK_SEARCH);
+            updateIsGoogleAppEnabled();
 
             if (getActivity() != null && !TextUtils.isEmpty(getPreferenceScreen().getTitle())) {
                 getActivity().setTitle(getPreferenceScreen().getTitle());
@@ -212,25 +210,6 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
 
         protected String getParentKeyForPref(String key) {
             return null;
-        }
-
-        /**
-         * Initializes a preference. This is called for every preference. Returning false here
-         * will remove that preference from the list.
-         */
-        protected boolean initPreference(Preference preference) {
-            switch (preference.getKey()) {
-                case KEY_ENABLE_MINUS_ONE:
-                    mShowGoogleAppPref = preference;
-                    updateIsGoogleAppEnabled();
-                    return true;
-                case Utilities.KEY_DOCK_SEARCH:
-                    mShowGoogleBarPref = preference;
-                    updateIsGoogleAppEnabled();
-                    return true;
-            }
-
-            return true;
         }
 
         private void updateIsGoogleAppEnabled() {

arcana-ext/platform_packages_apps_Launcher3

src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -176,12 +176,10 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
             setPreferencesFromResource(R.xml.launcher_home_screen_preferences, rootKey);
 
             PreferenceScreen screen = getPreferenceScreen();
-            for (int i = screen.getPreferenceCount() - 1; i >= 0; i--) {
-                Preference preference = screen.getPreference(i);
-                if (!initPreference(preference)) {
-                    screen.removePreference(preference);
-                }
-            }
+
+            mShowGoogleAppPref = screen.findPreference(KEY_MINUS_ONE);
+            mShowGoogleBarPref = screen.findPreference(Utilities.KEY_DOCK_SEARCH);
+            updateIsGoogleAppEnabled();
 
             if (getActivity() != null && !TextUtils.isEmpty(getPreferenceScreen().getTitle())) {
                 getActivity().setTitle(getPreferenceScreen().getTitle());
@@ -211,25 +209,6 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
 
         protected String getParentKeyForPref(String key) {
             return null;
-        }
-
-        /**
-         * Initializes a preference. This is called for every preference. Returning false here
-         * will remove that preference from the list.
-         */
-        protected boolean initPreference(Preference preference) {
-            switch (preference.getKey()) {
-                case KEY_MINUS_ONE:
-                    mShowGoogleAppPref = preference;
-                    updateIsGoogleAppEnabled();
-                    return true;
-                case Utilities.KEY_DOCK_SEARCH:
-                    mShowGoogleBarPref = preference;
-                    updateIsGoogleAppEnabled();
-                    return true;
-            }
-
-            return true;
         }
 
         private void updateIsGoogleAppEnabled() {